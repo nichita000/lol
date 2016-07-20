8ca11b5870c692bbd0a1bbb6cc962a2424b63994
nichita000 <nichita.ursu.v@gmail.com>
2016-07-20 15:12:36 +0300

    dadadada

--- chapter_1/lesson3/template.js
+++ chapter_1/lesson3/template.js
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-    <script>
         function Template(stra, propa) {
             this.str = stra;
             this.arr = propa;
@@ -59,7 +47,3 @@
         }
 
         console.log(obj.parse(star, lol));
-    </script>
-</body>
-
-</html>
--- chapter_2/lesson_1/js/script.js
+++ chapter_2/lesson_1/js/script.js
@@ -1,4 +1,4 @@
-function get(url, succ, err) {
+function get(url, succ) {
     var a = new XMLHttpRequest();
     a.open('GET', url, true);
     a.send();
@@ -6,8 +6,6 @@ function get(url, succ, err) {
         if (this.readyState === 4) {
             if (this.status === 200) {
                 succ(a.responseText);
-            } else {
-                err;
             }
         }
     }
@@ -19,9 +17,13 @@ for (var i = 0; i < a.length; i++) {
     a[i].addEventListener('click', function() {
         var tplName = this.dataset.tmpname;
         get('tmp/' + tplName + '.html', function(tpl) {
-            var e = document.getElementById('content-tmpl');
+            var e = document.querySelector('#content-tmpl #tpl');
             e.innerHTML = tpl;
-        })
+        });
+        get('http://localhost:3333/entry', function(repa) {
+            var e = document.querySelector('#content-tmpl #data');
+            e.innerHTML = repa;
+        });
     })
 }
 
